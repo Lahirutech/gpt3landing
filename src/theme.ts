bca8a59d3db10e2de291149c25d39a6969a3ec9f
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-26 07:23:34 +0530

    modify theme and palette

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,20 @@
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { palette } from './theme/palette';
 
-// Create a theme instance.
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
+  palette: palette,
+  typography: {
+    allVariants: {
+      color: 'white',
+      fontFamily: roboto.style.fontFamily,
     },
   },
 });
